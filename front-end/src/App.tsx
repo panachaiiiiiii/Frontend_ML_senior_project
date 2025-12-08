fa6288eba89fd0ff6f9c24e88ec09400242868be
panachaiiiiiii <gasza60@gmail.com>
2025-12-09 00:15:02 +0700

    Setup

---
+++ front-end/src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './Home'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
---
+++ front-end/src/Layout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <div>
+      <header>Header</header>
+
+      <main>
+        <Outlet />
+      </main>
+
+      <footer>Footer</footer>
+    </div>
+  )
+}
+
+export default Layout
