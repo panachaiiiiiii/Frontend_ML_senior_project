7a870224c619a900a5156ce36a02abf5414ebba0
panachaiiiiiii <gasza60@gmail.com>
2026-03-24 19:49:01 +0700

    Update : Move_backend

--- back-end/core/firebase_config.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, db
-
-cred = credentials.Certificate("serviceAccountKey.json")
-
-firebase_admin.initialize_app(cred, {
-    "databaseURL": "https://senior-b3690-default-rtdb.asia-southeast1.firebasedatabase.app"
-})
